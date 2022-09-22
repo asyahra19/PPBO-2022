3d2abd465d274100ef1148d09ad3dde0b7fff87b
asyahra19 <90130931+asyahra19@users.noreply.github.com>
2022-09-22 18:04:09 +0700

    Add files via upload

---
+++ Praktikum 3/PPBO_03_Latihan4.java
@@ -0,0 +1,17 @@
+// Nama : Asyahra Rahmasari Ariyahaq  
+// NIM  : M0521013
+
+public class PPBO_03_Latihan4 {
+    public static void main(String[] args) {
+        int i = 0;
+        int arr[] = {5, 1, 4, 2, 3};
+        do {
+            System.out.println("elemen ke-" + i + " = " + arr[i]);
+            i++;
+        }
+        while(i < 5);
+    }
+}
+
+// Melanjutkan source code Latihan3 yang sudah diperbaiki, ubahlah source code dengan 
+// mengganti dari perulangan FOR menjadi menggunakan DO ... WHILE
